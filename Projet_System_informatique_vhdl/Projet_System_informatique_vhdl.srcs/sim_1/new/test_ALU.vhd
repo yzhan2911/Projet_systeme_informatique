8274ea17c78aa8050bfea0aa122edb8f063b037c
Zhang Yu <yzhang5@insa-20929>
2024-05-03 11:33:39 +0200

    ALU fini

---
+++ Projet_System_informatique_vhdl/Projet_System_informatique_vhdl.srcs/sim_1/new/test_ALU.vhd
@@ -0,0 +1,145 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 05/03/2024 10:47:21 AM
+-- Design Name: 
+-- Module Name: test_ALU - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity test_ALU is
+end test_ALU;
+
+architecture Behavioral of test_ALU is
+ component ALU
+    Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
+           B : in STD_LOGIC_VECTOR (7 downto 0);
+           S : out STD_LOGIC_VECTOR (7 downto 0);
+           Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0);
+           N : out STD_LOGIC;
+           O : out STD_LOGIC;
+           Z : out STD_LOGIC;
+           C : out STD_LOGIC);
+    end component ;
+    signal A : STD_LOGIC_VECTOR (7 downto 0);
+    signal B : STD_LOGIC_VECTOR (7 downto 0);
+    signal Ctrl_Alu : STD_LOGIC_VECTOR (2 downto 0);
+    signal S : STD_LOGIC_VECTOR (7 downto 0);
+    signal N, O, Z, C : STD_LOGIC;
+begin
+    uut: ALU 
+    port map(
+    A => A,
+    B => B,
+    Ctrl_Alu => Ctrl_Alu,
+    S => S,
+    N => N,
+    O => O,
+    Z => Z,
+    C => C
+    );
+ -- Test process
+    process
+    begin
+    -- Test 1: Addition
+    A <= "00000001"; -- 1
+    B <= "00000001"; -- 1
+    Ctrl_Alu <= "001"; -- ADD
+    wait for 10 ns;
+    
+    assert S = "00000010" and C = '0' and O = '0' and N = '0' and Z = '0'
+    report "Test Addition failed" severity error;
+    
+
+    
+    -- Test 2: Subtraction
+    A <= "00000010"; -- 2
+    B <= "00000001"; -- 1
+    Ctrl_Alu <= "010"; -- SUB
+    wait for 10 ns;
+    
+    assert S = "00000001" and C = '0' and O = '0' and N = '0' and Z = '0'
+    report "Test Subtraction failed" severity error;
+    
+    -- Test 3: Multiplication
+    A <= "00000010"; -- 2
+    B <= "00000011"; -- 3
+    Ctrl_Alu <= "011"; -- MUL
+    wait for 10 ns;
+    
+    assert S = "00000110" and C = '0' and O = '0' and N = '0' and Z = '0'
+    report "Test Multiplication failed" severity error;
+       
+    -- Test 4: Division
+    A <= "00000110"; -- 6
+    B <= "00000011"; -- 3
+    Ctrl_Alu <= "100"; -- DIV
+    wait for 10 ns;
+    
+    assert S = "00000010" and C = '0' and O = '0' and N = '0' and Z = '0'
+    report "Test Division failed" severity error;
+    
+    
+     -----test carry 
+    A <= "11111111"; 
+    B <= "11111111"; 
+    Ctrl_Alu <= "001"; 
+    wait for 10 ns;
+
+    assert C = '1' and O = '0' and N = '0' and Z = '0'
+    report "Test carry failed" severity error;
+    
+    -----test overflow 
+    A <= "11111111"; 
+    B <= "11111111"; 
+    Ctrl_Alu <= "011"; 
+    wait for 10 ns;
+ 
+    assert C = '0' and O = '1' and N = '0' and Z = '0'
+    report "Test overflow failed" severity error;
+    
+    -----test Zeros 
+    A <= "11111111"; 
+    B <= "00000000"; 
+    Ctrl_Alu <= "100"; 
+    wait for 10 ns;
+ 
+    assert C = '0' and O = '0' and N = '0' and Z = '1'
+    report "Test Zeros failed" severity error;
+    
+    -----test negative 
+    A <= "00000001"; 
+    B <= "00000011"; 
+    Ctrl_Alu <= "010"; 
+    wait for 10 ns;
+ 
+    assert C = '0' and O = '0' and N = '1' and Z = '0'
+    report "Test negative failed" severity error;
+    
+    wait;
+    end process;
+end Behavioral;
--- Projet_System_informatique_vhdl/Projet_System_informatique_vhdl.srcs/sources_1/new/ALU.vhd
+++ Projet_System_informatique_vhdl/Projet_System_informatique_vhdl.srcs/sources_1/new/ALU.vhd
@@ -35,7 +35,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity ALU is
 Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
            B : in STD_LOGIC_VECTOR (7 downto 0);
-           Ctrl_Alu : in STD_LOGIC_VECTOR (1 downto 0);  
+           Ctrl_Alu : in STD_LOGIC_VECTOR (2 downto 0);  
            S : out STD_LOGIC_VECTOR (7 downto 0);
            N : out STD_LOGIC;
            O : out STD_LOGIC;
@@ -43,34 +43,47 @@ Port ( A : in STD_LOGIC_VECTOR (7 downto 0);
            C : out STD_LOGIC
            );
 end ALU;
--- Instruction code
     -- ADD    "01"    
     -- MUL    "02"
     -- SUB    "03"
     -- DIV    "04"
-architecture Behavioral of ALUU is
-    signal S_16bits,A_16bits,B_16bits : STD_LOGIC_VECTOR (15 downto 0); --16 bits
+architecture Behavioral of ALU is
+    signal S_16bits,A_16bits,B_16bits : STD_LOGIC_VECTOR (15 downto 0):=(others =>'0'); --16 bits
 begin
-    process(A,B,Ctrl_Alu)
+
+    process(A_16bits, B_16bits,A,B,Ctrl_Alu)
         begin
-        A_16bits<=x"00"& A;B_16bits<=x"00"&B; --transferer en 16 bits
-        case Control is 
-           when "01"=> ---addtion
-           S_16bits <= A_16bits + B_16bits;
-           when "02"=> ---soustraction
-           S_16bits <= A_16bits - B_16bits;
-           when "03"=> --- multiplication
+        A_16bits<=x"00"&A;
+        B_16bits<=x"00"&B; --transferer en 16 bits
+        N<= '0';
+        O<='0';
+        Z<='0';
+        C<='0';
+        case Ctrl_Alu is 
+           when "001"=> ---addtion
+               S_16bits <= A_16bits + B_16bits;
+               if(A_16bits + B_16bits> 255) then C<='1';
+               elsif (A_16bits + B_16bits=0) then Z<='1';
+               end if;
+           when "010"=> ---soustraction
+               S_16bits <= A_16bits - B_16bits;
+               if (B_16bits > A_16bits) then N<='1';
+               elsif (B_16bits = A_16bits) then Z<='1';
+               end if;
+           when "011"=> --- multiplication
            S_16bits <= A*B;
-           when "04"=> --division
-           if (B /= 0) then S_16bits <= ( std_logic_vector(to_unsigned(to_integer(unsigned(A_16bits)) / to_integer(unsigned(B_16bits)),16))); 
-           else S_16bits <= x"0000"; 
+           if (A*B>255)then O<='1';
+           elsif(A*B=0) then Z<='1';
            end if;
+           when "100"=> --division
+           if (B /= 0) then S_16bits <= ( std_logic_vector(to_unsigned(to_integer(unsigned(A_16bits)) / to_integer(unsigned(B_16bits)),16))); 
+           else S_16bits <= x"0000";Z<='1'; 
+           end if;
+           when others => -- Ajout du choix par défaut
+            S_16bits <= (others => '0'); 
            end case;
         end process;
-            C <= '1' when S_16bits(15 downto 8) /= x"00" and Ctrl_Alu = "01" else '0';--- Carry pour addition
-            O <= '1' when S_16bits(15 downto 8) /= x"00"  else '0'; --- overflow
-            N <= '1' when S_16bits(15)=='1' else '0'; ---negative
-            Z <= '1' when S_16bits(15 downto 0) ==x"00" else '0'; --- zeros
+            
             S <= S_16bits(7 downto 0 ) ; 
 
 end Behavioral;
