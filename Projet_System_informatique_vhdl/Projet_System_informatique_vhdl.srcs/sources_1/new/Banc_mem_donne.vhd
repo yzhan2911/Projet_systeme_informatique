2ff4dcacc3510843f76c9a959a760c9e9252d05a
ZYJ <yongjiazeng@yongjias-MacBook-Air.local>
2024-05-06 08:34:34 +0200

    memoire de donnee

--- Projet_System_informatique_vhdl/Projet_System_informatique_vhdl.srcs/sources_1/new/Banc_mem_donne.vhd
+++ Projet_System_informatique_vhdl/Projet_System_informatique_vhdl.srcs/sources_1/new/Banc_mem_donne.vhd
@@ -21,10 +21,11 @@
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -41,8 +42,19 @@ entity Banc_mem_donne is
 end Banc_mem_donne;
 
 architecture Behavioral of Banc_mem_donne is
-
+    type Memoire_tab is array (0 to 4) of STD_LOGIC_VECTOR (7 downto 0);
+    signal Memoire : Memoire_tab := (others => x"00");
 begin
-
+    process
+    begin
+        wait until clk'event and clk = '1';     --synchrones avec CLK.
+        if RST = '1' then              -- Reset
+            Memoire <= (others => x"00"); 
+        elsif RW = '0' then          --écriture
+                Memoire(to_integer(unsigned(add))) <= Data_in;
+            end if;
+        end if;
+    end process;
+    dataOUT <= Memoire(to_integer(unsigned(add))); --reading 
 
 end Behavioral;
